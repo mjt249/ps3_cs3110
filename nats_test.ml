68160c3c5afcd65e8ef52fd4e33ce50c7880fc08
mjt249 <mjt249@cornell.edu>
2014-09-27 09:25:35 -0400

    more testing of nats

--- nats_test.ml
+++ nats_test.ml
@@ -13,10 +13,31 @@ let b = nat_of_int y in
 let newx = int_of_nat a in
 let newy = int_of_nat b in
 assert_true ((x = newx) && (y = newy))
-
-(* assert_raises (Some (Failure "")) eval (make_fact_tree (-1)) *)
-
-
+let x = zero in
+let newx = int_of_nat x in
+assert_true (0 = newx)
+let x = one in
+let newx = int_of_nat x in
+assert_true (1 = newx)
+let x = zero in
+let newx = nat_of_int 0 in
+assert_true (x = newx)
+let x = one in
+let newx = nat_of_int 1 in
+assert_true (x = newx)
+let x = -5 in
+assert_raises (Some Unrepresentable) nat_of_int x
+let x = nat_of_int 3 in
+let y = nat_of_int 5 in
+let rslt = ( + ) x y in
+assert_true ((int_of_nat rslt)  = 8)
+let x = nat_of_int 3 in
+let y = nat_of_int 5 in
+let rslt = ( * ) x y in
+assert_true ((int_of_nat rslt)  = 15)
+let x = nat_of_int 461168601 in
+let y = nat_of_int 461168602 in
+assert_raises (Some Unrepresentable) (( * ) x y)
 
 
 
