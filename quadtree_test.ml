c14dc9aebf76b14439e1932d49dbc574483d265f
Jay Jung <cj329@cornell.edu>
2014-09-28 22:08:05 -0400

    still need testing for min_diag case

--- quadtree_test.ml
+++ quadtree_test.ml
@@ -7,6 +7,7 @@ let two_objects = insert one_object (-4.0, 4.0) 1
 let three_objects = insert two_objects (-4.0, -4.0) 1
 let four_objects = insert three_objects (4.0, -4.0) 1
 let another_level = insert four_objects (2.0, 2.0) 1
+let in_the_center = insert another_level (0.0, 0.0) 1
 let count_obj (acc : int) (tuple : coord * 'b) : int =
   acc + 1 
 
@@ -14,4 +15,16 @@ TEST_UNIT "insert_test1" = assert_true ((fold_quad count_obj 0 one_object) = 1)
 TEST_UNIT "insert_test2" = assert_true ((fold_quad count_obj 0 two_objects) = 2)
 TEST_UNIT "insert_test3" = assert_true ((fold_quad count_obj 0 three_objects) = 3)
 TEST_UNIT "insert_test4" = assert_true ((fold_quad count_obj 0 four_objects) = 4)
-(*TEST_UNIT "insert_test5" = assert_true ((fold_quad count_obj 0 another_level) = 5) *)
+TEST_UNIT "insert_test5" = assert_true ((fold_quad count_obj 0 another_level) = 5)
+TEST_UNIT "insert_test6" = assert_true ((fold_quad count_obj 0 in_the_center) = 6)
+
+TEST_UNIT "fold_region_test1" = 
+  assert_true ((fold_region count_obj 0 one_object ((-8.0, -8.0), (0.0, 0.0)) = 0))
+TEST_UNIT "fold_region_test2" = 
+  assert_true ((fold_region count_obj 0 one_object ((-8.0, -8.0), (8.0, 8.0)) = 1))
+TEST_UNIT "fold_region_test3" = 
+  assert_true ((fold_region count_obj 0 four_objects ((-8.0, -8.0), (0.0, 8.0)) = 2))
+TEST_UNIT "fold_region_test4" = 
+  assert_true ((fold_region count_obj 0 four_objects ((-8.0, -8.0), (7.0, 8.0)) = 4))
+TEST_UNIT "fold_region_test5" = 
+  assert_true ((fold_region count_obj 0 in_the_center ((-5.0, -5.0), (0.0, 0.0)) = 2))
